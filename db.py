e59a915d01bd219c0022c92036fa0471ec2bbcb8
KenanMathews <kenan.mathukutty@gmail.com>
2024-03-26 23:52:28 +0530

    env load fix

--- db.py
+++ db.py
@@ -3,8 +3,8 @@ from supabase import create_client, Client
 import os
 
 # Initialize Supabase client
-SUPABASE_URL = os.getenv("SUPABASE_KEY")
-SUPABASE_KEY = os.getenv("SUPABASE_URL")
+SUPABASE_URL = os.getenv("SUPABASE_URL")
+SUPABASE_KEY = os.getenv("SUPABASE_KEY")
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 
 import random
